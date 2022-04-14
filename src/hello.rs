7d77338862fc216538357f7454151a8e0e3418fd
keketon <mas08rin10yabko@gmail.com>
2022-04-14 22:30:00 +0900

    std::Debug

--- src/hello.rs
+++ src/hello.rs
@@ -1,3 +1,20 @@
 fn main() {
-  print!("hello world!");
+  println!("{} days", 31);
+  println!("{:?} days", 31);
+  // println!("{} is invalid UnPrintable", UnPrintable(1));
+  // println!("{:?} is UnPrintable", UnPrintable(2));
+  // println!("{} is invalid Printable", Printable(3));
+  println!("{:?} is Printable", Printable(4));
+  println!("{:#?} is Printable", Printable(4));
+  // println!("{} is invalid Deep", Deep(Printable(5)));
+  println!("{:?} is Deep", (Deep(Printable(6))));
+  println!("{:#?} is Deep", (Deep(Printable(6))));
 }
+
+struct UnPrintable (i32);
+
+#[derive(Debug)]
+struct Printable (i32);
+
+#[derive(Debug)]
+struct Deep (Printable);
