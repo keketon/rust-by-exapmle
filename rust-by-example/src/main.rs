cfea91b9fbdd45b0ba5dca0a3ce5b01a380d4d69
keketon <mas08rin10yabko@gmail.com>
2022-04-20 23:39:55 +0900

    rollback to hello world

--- rust-by-example/src/main.rs
+++ rust-by-example/src/main.rs
@@ -1,15 +1,3 @@
-use std::io;
-
 fn main() {
-  let mut input = String::new();
-  io::stdin()
-    .read_line(&mut input)
-    .expect("Failed to read line.");
-
-  let mut sum = 45;
-
-  for ch in input.iter() {
-    let num = ch as usize - '0' as usize;
-    usd[num] = true;
-  }
+  println!("hello world");
 }
