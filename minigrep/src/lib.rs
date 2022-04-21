b6b2f7e2843c1d8e383a2b8786d3a9b365a2ac03
keketon <mas08rin10yabko@gmail.com>
2022-04-21 22:31:17 +0900

    use stderr

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,10 +1,12 @@
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
+use std::env;
 
 pub struct Config {
   pub query: String,
   pub filename: String,
+  pub case_sensitive: bool,
 }
 
 impl Config {
@@ -16,7 +18,9 @@ impl Config {
     let query = args[1].clone();
     let filename = args[2].clone();
 
-    Ok(Config { query, filename })
+    let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+    Ok(Config { query, filename, case_sensitive })
   }
 }
 
@@ -26,7 +30,13 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   let mut contents = String::new();
   f.read_to_string(&mut contents)?;
 
-  for line in search(&config.query, &contents) {
+  let results = if config.case_sensitive {
+    search(&config.query, &contents)
+  } else {
+    search_case_insensitive(&config.query, &contents)
+  };
+
+  for line in results {
     println!("{}", line);
   }
 
@@ -58,7 +68,6 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a st
   results
 }
 
-
 #[cfg(test)]
 mod test {
   use super::*;
@@ -84,6 +93,9 @@ safe, fast, productive.
 Pick three.
 Trust me.";
 
-    assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
+    assert_eq!(
+      vec!["Rust:", "Trust me."],
+      search_case_insensitive(query, contents)
+    );
   }
 }
