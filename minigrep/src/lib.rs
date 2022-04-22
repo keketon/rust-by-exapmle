51b25c65480726382c98a5c6cdefffebfd384eb1
keketon <mas08rin10yabko@gmail.com>
2022-04-23 00:45:34 +0900

    modify minigrep by itr

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,7 +1,7 @@
+use std::env;
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
-use std::env;
 
 pub struct Config {
   pub query: String,
@@ -10,17 +10,25 @@ pub struct Config {
 }
 
 impl Config {
-  pub fn new(args: &[String]) -> Result<Config, &'static str> {
-    if args.len() < 3 {
-      return Err("not enogh arguments");
-    }
+  pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+    args.next();
 
-    let query = args[1].clone();
-    let filename = args[2].clone();
+    let query = match args.next() {
+      Some(arg) => arg,
+      None => return Err("Didn't get a query string"),
+    };
+    let filename = match args.next() {
+      Some(arg) => arg,
+      None => return Err("Didn't get a filename"),
+    };
 
     let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-    Ok(Config { query, filename, case_sensitive })
+    Ok(Config {
+      query,
+      filename,
+      case_sensitive,
+    })
   }
 }
 
@@ -44,17 +52,26 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-  let mut results = Vec::new();
-
-  for line in contents.lines() {
-    if line.contains(query) {
-      results.push(line);
-    }
-  }
-
-  results
+  contents
+    .lines()
+    .filter(|line| line.contains(query))
+    .collect()
 }
 
+/// # Example
+/// 
+/// ```
+/// let query = "hoge";
+/// let contents = "\
+/// hoge
+/// fuga
+/// ";
+/// 
+/// assert_eq!(
+///   vec!["hoge"],
+///   minigrep::search_case_insensitive(query, contents)
+/// );
+/// ```
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
   let query = query.to_lowercase();
   let mut results = Vec::new();
